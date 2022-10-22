a440802e481ca1dd61c7880f15b18e269f463066
ArneRobot <90373987+ArneRobot@users.noreply.github.com>
2022-10-23 00:53:10 +0200

    Jeg hater meg selv
    jeg angrer på at jeg bestemte meg for dette, jeg skal drepe personen som fant opp Promise()

--- app.js
+++ app.js
@@ -27,31 +27,40 @@ function generer() {
     // leter etter :tag: og bytter det ut med tilfeldig fra liste
     tid = tid.replace(/:offer:/g, offere[Math.floor(Math.random()*offere.length)]);
     tid = tid.replace(/:handling:/g, handlinger[Math.floor(Math.random()*handlinger.length)]);
+
     // velger kjønnet på personen og erstatter :kjoenn: med riktig pronomen
-    let inpKjoenn = document.getElementById('kjoenn').value
-    if (inpKjoenn == 'male') {
-        kjoenn = 'han'
-    }
-    else if (inpKjoenn == 'female') {
-        kjoenn = 'hun'
-    }
-    tid = tid.replace(/:kjoenn:/g, kjoenn)
-    handling = handling.replace(/:kjoenn:/g, kjoenn)
-
-    // setter sammen setning
-    let vits = navn + ' ' + handling + ' ' + offer + ' ' + tid;
-
-    // lager elementet som holder sitatet
-    let sitat = document.createElement('p');
-    sitat.innerText = '"' + vits + '"';
-    sitat.style = 'color: #FEFFFF; ';
-    sitat.id = 'sitatTekst';
-    document.getElementById('sitat').appendChild(sitat);
-    // lager "-Brian"
-    let brian = document.createElement('p');
-    brian.innerText = '-Brian';
-    brian.style = 'text-align: right; margin-right: 1em;';
-    document.getElementById('sitat').appendChild(brian);
+    try {
+        fetch('https://api.genderize.io?name='+navn)
+        .then(response => response.json())
+        .then(data => {
+            let pronomen;
+            if (data['gender'] == 'female') {
+                pronomen = 'hun';
+            } else if (data['gender'] == 'male') {
+                pronomen = 'han';
+            } else {
+                pronomen = 'de';
+            }
+            tid = tid.replace(/:kjoenn:/g, pronomen);
+            handling = handling.replace(/:kjoenn:/g, pronomen);
+            
+            // setter sammen setning
+            let vits = navn + ' ' + handling + ' ' + offer + ' ' + tid;
+                    
+            // lager elementet som holder sitatet
+            let sitat = document.createElement('p');
+            sitat.innerText = '"' + vits + '"';
+            sitat.style = 'color: #FEFFFF; ';
+            sitat.id = 'sitatTekst';
+            document.getElementById('sitat').appendChild(sitat);
+            
+            // lager "-Brian"
+            let brian = document.createElement('p');
+            brian.innerText = '-Brian';
+            brian.style = 'text-align: right; margin-right: 1em;';
+            document.getElementById('sitat').appendChild(brian);
+        })
+    } catch (error) {console.log(error)}
 }
 
 // tar inn en setning (navn) og gjør forbokstaven av hvert ord stort
