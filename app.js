234b53a194c19903ecd4e40e87bf9dd4dad53644
ArneRobot <90373987+ArneRobot@users.noreply.github.com>
2022-10-30 20:51:57 +0100

    darkmode
    forbedret UI-en til darkmode

--- app.js
+++ app.js
@@ -106,12 +106,11 @@ function capitalize(navn) {
     return navn;
 }
 
-let modeArray = ['images/darkMode.png', 'DARK', 'LIGH', 'images/lightMode.png']
+let modeArray = ['images/darkMode.png', 'images/lightMode.png']
 
 function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
     document.getElementById('modeBilde').src = modeArray[0];
-    document.getElementById('modeTekst').innerText = modeArray[1];
     modeArray.reverse();
 }
